3af2b57af59f1338813ad8168f77f8a2257a3b5a
limeng0403 <limeng0403@gmail.com>
2016-05-05 15:15:13 +0800

    Add css and js.

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(function() {
+    var currInx = 1;
+    var itemLen = $('.item').length;
+
+    $('a.iconfont-left').on('click', function() {
+        aClickEvent('left');
+
+        $('.item').first().css('margin-left', '-50%');
+        $('.box').append($('.item').first().css('margin-left', 0));
+    });
+    $('a.iconfont-right').on('click', function() {
+        aClickEvent('right');
+
+        $('.item').first().before($('.item').last().css('margin-left', '-50%'));
+        $('.item').first().css('margin-left', 0);
+    });
+
+    function aClickEvent(lor) {
+        if (lor = 'left') {
+            currInx++;
+            if (currInx > itemLen) {
+                currInx = 1;
+            }
+        } else {
+            currInx--;
+            if (currInx <= 0) {
+                currInx = itemLen;
+            }
+        }
+
+        $('.list li').css('border', 'none');
+        $('.list li').eq(currInx - 1).css('border', '1px solid #eee');
+    }
+});
